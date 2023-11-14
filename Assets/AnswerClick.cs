7ac38a6700ba968706bf56685d40d57c62e9bdef
Aidan Nusbaum <aidnus329@gmail.com>
2023-11-14 01:36:39 -0600

    need event triggers to be added to buttons

---
+++ Assets/AnswerClick.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8703c18f8a6db99dcfafa8cdf6eac95439dff1092226accc538ed199b7c8db50
+size 2184
--- Assets/Question.cs
+++ Assets/Question.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c43aca1a1da7bd0d9801f9d4a0cfbef60055cd024b14878843dce6723a652a9
-size 760
+oid sha256:9d8a3448aa9ab87d61a50f4edde67857428d73a6a1e7d4b9d5ce37bc4653edac
+size 887
